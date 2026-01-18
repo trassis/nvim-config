48babd4f7cb9e478d39db308f16473f93d44e75a
Thiago Assis <essemeia@gmail.com>
2026-01-18 10:42:05 -0300

    add instructions in lsp file

--- lua/trassis/plugins/lspconfig.lua
+++ lua/trassis/plugins/lspconfig.lua
@@ -1,14 +1,17 @@
+-- To add a new language server, follow the steps:
+-- 1) Install the server with Mason (:MasonInstall 'server-name')>
+-- 2) Enable the server bellow.
+-- Servers names are available in 'https://github.com/neovim/nvim-lspconfig/tree/master/lsp'.
+
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    -- Enable the servers here (names are available in https://github.com/neovim/nvim-lspconfig/tree/master/lsp).
     vim.lsp.enable('lua_ls')
     vim.lsp.enable('pyright')
     vim.lsp.enable('clangd')
     vim.lsp.enable('julials')
     vim.lsp.enable('tinymist')
-
-    -- Frontend
+    -- Frontend languages
     vim.lsp.enable('ts_ls')
     vim.lsp.enable('html')
     vim.lsp.enable('cssls')
